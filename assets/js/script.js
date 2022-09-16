9b7fcaa386cba345b345cbfff0d98502a2d8c1cd
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2022-09-16 13:07:47 -0300

    Mudar de página

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
-alert("Eu sou um alert!");
-document.getElementById("teste").innerHTML = "Hello JavaScript";
+let qs = (valor) => document.querySelector(valor);
+function keydownFunction(event, enterobj) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        let nameHome = qs("#search");
+        let vlrHome = nameHome.value;
+        nextPage(vlrHome);
+        //qs('#search_rst').value = 'ResultNew';
+        //window.location.href = "pages/result.html";  
+    }  
+}
+function nextPage(vlrHome){
+    qs('#lupaHome').click();
+    console.log(vlrHome);
+}
+
+
+
